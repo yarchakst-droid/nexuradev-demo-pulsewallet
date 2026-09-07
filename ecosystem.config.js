f97c4cb410f759a6f8edf5f88525f3dc173d98fe
yarchak.st <yarchak.st@gmail.com>
2026-09-08 00:51:21 +0200

    chore(deploy): set PM2 max_memory_restart based on server RAM budget

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,6 +5,7 @@ module.exports = {
       cwd: __dirname,
       script: "node_modules/.bin/next",
       args: "start -p 3214 -H 127.0.0.1",
+      max_memory_restart: "300M",
       env: {
         NODE_ENV: "production",
       },
